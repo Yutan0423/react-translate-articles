64be3ac143e325df216501d300ab9f9e775b2e0c
Yutan0423 <yuutan3240@gmail.com>
2021-05-23 22:11:55 +0900

    create output area

--- public/js/app.js
+++ public/js/app.js
@@ -39286,24 +39286,76 @@ if (document.getElementById("app")) {
 "use strict";
 
 
-var __importDefault = this && this.__importDefault || function (mod) {
-  return mod && mod.__esModule ? mod : {
-    "default": mod
-  };
+var __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  Object.defineProperty(o, k2, {
+    enumerable: true,
+    get: function get() {
+      return m[k];
+    }
+  });
+} : function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  o[k2] = m[k];
+});
+
+var __setModuleDefault = this && this.__setModuleDefault || (Object.create ? function (o, v) {
+  Object.defineProperty(o, "default", {
+    enumerable: true,
+    value: v
+  });
+} : function (o, v) {
+  o["default"] = v;
+});
+
+var __importStar = this && this.__importStar || function (mod) {
+  if (mod && mod.__esModule) return mod;
+  var result = {};
+  if (mod != null) for (var k in mod) {
+    if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+  }
+
+  __setModuleDefault(result, mod);
+
+  return result;
 };
 
 Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 
-var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+var react_1 = __importStar(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 
 var react_2 = __webpack_require__(/*! @chakra-ui/react */ "./node_modules/@chakra-ui/react/dist/cjs/index.js");
 
+var react_3 = __webpack_require__(/*! @chakra-ui/react */ "./node_modules/@chakra-ui/react/dist/cjs/index.js");
+
+var react_4 = __webpack_require__(/*! @chakra-ui/react */ "./node_modules/@chakra-ui/react/dist/cjs/index.js");
+
 var CreateArticle = function CreateArticle() {
+  var _a = react_1.useState(""),
+      inputText = _a[0],
+      setInputText = _a[1];
+
+  var _b = react_1.useState(""),
+      outputText = _b[0],
+      setOutputText = _b[1];
+
+  var onChangeTranslate = function onChangeTranslate(e) {
+    setInputText(e.target.value);
+  };
+
+  var onClickTranslate = function onClickTranslate() {
+    setOutputText(inputText);
+  };
+
   return react_1["default"].createElement(react_1["default"].Fragment, null, react_1["default"].createElement("h2", null, "Translate Articles"), react_1["default"].createElement(react_2.Textarea, {
+    value: inputText,
+    onChange: onChangeTranslate,
     placeholder: "enter English sentences"
-  }));
+  }), react_1["default"].createElement("p", null, outputText), react_1["default"].createElement(react_4.Button, {
+    onClick: onClickTranslate
+  }, "Translate"), react_1["default"].createElement(react_3.Container, null));
 };
 
 exports.default = CreateArticle;
--- resources/ts/components/CreateArticle.tsx
+++ resources/ts/components/CreateArticle.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Textarea } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 const CreateArticle = () => {
+    const [inputText, setInputText] = useState("");
+    const [outputText, setOutputText] = useState("");
+
+    const onChangeTranslate = (e: any) => {
+        setInputText(e.target.value);
+    };
+
+    const onClickTranslate = () => {
+        setOutputText(inputText);
+    };
+
     return (
         <>
             <h2>Translate Articles</h2>
-            <Textarea placeholder="enter English sentences" />
+            <Textarea
+                value={inputText}
+                onChange={onChangeTranslate}
+                placeholder="enter English sentences"
+            />
+            <p>{outputText}</p>
+            <Button onClick={onClickTranslate}>Translate</Button>
+            <Container></Container>
         </>
     );
 };
