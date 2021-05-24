173f63047e13f8eae051955b500c677ed743e840
Yutan0423 <yuutan3240@gmail.com>
2021-05-24 23:20:02 +0900

    apply azure translator

--- resources/ts/components/CreateArticle.tsx
+++ resources/ts/components/CreateArticle.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+// const { v4: uuidv4 } = require("uuid");
 import React, { useState } from "react";
 import { Textarea } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
@@ -7,8 +9,39 @@ const CreateArticle = () => {
     const [inputText, setInputText] = useState("");
     const [outputText, setOutputText] = useState("");
 
-    const onChangeTranslate = (e: any) => {
+    const onChangeTranslate = async (e: any) => {
         setInputText(e.target.value);
+        var subscriptionKey = "4785173cce9c47259d27712d4b461f3c";
+        let endpoint = "https://api.cognitive.microsofttranslator.com";
+
+        // Add your location, also known as region. The default is global.
+        // This is required if using a Cognitive Services resource.
+        let location = "japaneast";
+
+        axios({
+            baseURL: endpoint,
+            url: "/translate",
+            method: "post",
+            headers: {
+                "Ocp-Apim-Subscription-Key": subscriptionKey,
+                "Ocp-Apim-Subscription-Region": location,
+                "Content-type": "application/json",
+                // "X-ClientTraceId": uuidv4().toString(),
+            },
+            params: {
+                "api-version": "3.0",
+                from: "en",
+                to: "ja",
+            },
+            data: [
+                {
+                    text: "My name is Yuta",
+                },
+            ],
+            responseType: "json",
+        }).then(function (response) {
+            console.log(JSON.stringify(response.data, null, 4));
+        });
     };
 
     const onClickTranslate = () => {
